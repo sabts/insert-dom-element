e6f39d6028428e902697bf0d4329a1ab9b74a839
sabts <sabrinataraschi@gmail.com>
2025-04-13 17:28:57 +0200

    ready

--- js/scripts.js
+++ js/scripts.js
@@ -46,7 +46,7 @@ const supplies = [
 
     const cars = ['🚗', '🚕', '🚙'];
 
-    let carsHistory = [];
+    const carsHistory = [];
 
 //1️⃣Crea un input donde el usuario pueda escribir su nombre y un botón con el texto "Say hello". Al pulsar el botón, se debe crear e insertar un elemento h2 con el texto "Hello, [nombre]".
 const greetings = event => {
@@ -63,48 +63,21 @@ const greetings = event => {
   }
 }
 
+
 //2️⃣Imagina que tienes un control deslizante que te permite elegir el nivel de poder de un encabezado del 1 al 6.
-const rangeValueInLabel = (event) =>{
+const rangeValueInLabel = (event) =>{ ///esto es para que se vea el valor en el que esta el range
   const rangeValue = event.target.value;
-  rangeLabelElement.textContent = rangeValue;}
+  rangeLabelElement.textContent = rangeValue;
+}
 
 const generateHRange = (event) => {
-if (rangeElement.value === '1') {
-  const h1Element = document.createElement('h1')
-  h1Element.textContent = "I am an h" + rangeElement.value;
-  divRangeElement.append(h1Element);
-  console.log("h1 :)")
-}
-else if (rangeElement.value === '2') {
-  const h2Element = document.createElement('h2')
-  h2Element.textContent = "I am an h" + rangeElement.value;
-  divRangeElement.append(h2Element);
-  console.log("h2 :)")
-}
-else if (rangeElement.value === '3') {
-  const h3Element = document.createElement('h3')
-  h3Element.textContent = "I am an h" + rangeElement.value;
-  divRangeElement.append(h3Element);
-  console.log("h3 :)")
-}
-else if (rangeElement.value === '4') {
-  const h4Element = document.createElement('h4')
-  h4Element.textContent = "I am an h" + rangeElement.value;
-  divRangeElement.append(h4Element);
-  console.log("h4 :)")
-}
-else if (rangeElement.value === '5') {
-  const h5Element = document.createElement('h5')
-  h5Element.textContent = "I am an h" + rangeElement.value;
-  divRangeElement.append(h5Element);
-  console.log("h5 generado :)")
-}
-else{
-  const h6Element = document.createElement('h6')
-  h6Element.textContent = "I am an h" + rangeElement.value;
-  divRangeElement.append(h6Element);
-  console.log("h6 generado :)")
-}
+
+    const hnumber = rangeElement.value; //asigno un valor hnumber
+    const hElement = document.createElement(`h${hnumber}`);  //que cree un documento de cada valor que puede tener en el range
+    hElement.textContent = `I am an h${hnumber}`
+    divRangeElement.append(hElement);
+
+    //console.log(`h${hnumber} generado :D`);
 }
 
 //3️⃣ 🎮 Abby y su búsqueda de suministros
@@ -129,28 +102,31 @@ const abbyFindingSupplies = () =>{
 
 //4️⃣ 🏃‍♀️💥 Bego Runner: Modo “Esquivar Coches” ON  //no lo he terminado, porque me trabe en el const de repating cars...
 const repeatingCars = (car) => {
- if(carsHistory.length === 3 && car === car) /// primero innte hacer una funcion generica para cada coche, pero la advertencia me sale al segundo carro igual
-  return warningTextElement.textContent =`Bego, please be careful! This car model ${car} is stalking you! 🚨` //queria que aqui me saliera el modelo del coche que se esta repidiendo pero siempre me sale el azul
+ if(carsHistory.length === 3 && carsHistory[0] === carsHistory[1] && carsHistory[1] === carsHistory[2]){
+  carsHistory.shift(car);
+  return warningTextElement.textContent =`Bego, please be careful! This car model ${car} is stalking you! 🚨`} else {
+    warningTextElement.textContent = "";
+  }
   }
 
-  //por los momentos esto genera sin problema los coches, pero como estoy con lo anterior, no he confirmado si el mensaje me da error o no
-  const begoWatchOut = () => {
+
+const begoWatchOut = () => {
    
     const carsBehindBego = document.createElement('p');
-  
     const randomCar = cars[Math.floor(Math.random() * cars.length)];
-    carsHistory.push(randomCar)
 
-    repeatingCars(cars[0]);
-    repeatingCars(cars[1]);
-    repeatingCars(cars[2]);
+    carsHistory.push(randomCar);
+
+    if (carsHistory.length > 3) {
+      carsHistory.shift();
+    }
   
+    
     carsBehindBego.textContent = randomCar;
     trafficZoneElement.append(carsBehindBego);
+    repeatingCars(randomCar);
 
-    //dangerButtonElement.addEventListener('click',() => {
-    //warningTextElement.textContent = ""}
-  };
+    }
 
 inputButtonElement.addEventListener("click", greetings);
 rangeElement.addEventListener("input",rangeValueInLabel)
